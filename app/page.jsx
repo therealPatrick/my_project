f90239daa6d73985a9f94f5a7ffd695a53b85fd6
therealPatrick <malamashangala13@gmail.com>
2024-04-08 17:24:14 +0200

    landing page layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Link from 'next/link'
-
+import Hero from '@/components/Hero'
+import InfoBoxes from '@/components/infoBoxes'
 const Homepage = () => {
     return (
-        <div>
-            <h1 className="text-3xl">Welcome</h1>
-            <Link href="/properties" className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-2'>Show Properties</Link>
-        </div>
+        <>
+            <Hero />
+            <InfoBoxes />
+        </>
     )
 }
 
