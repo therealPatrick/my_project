c2c9bc3ddeb20e92a5f46af697a92eaf6f122550
therealPatrick <malamashangala13@gmail.com>
2024-04-16 13:20:23 +0200

    recent pages done

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,16 @@
 import Hero from '@/components/Hero'
 import InfoBoxes from '@/components/infoBoxes'
 import Footer from '@/components/Footer'
+import HomeProperties from '@/components/HomeProperties'
+
+
 
 const Homepage = () => {
     return (
         <>
             <Hero />
             <InfoBoxes />
+            <HomeProperties />
             <Footer />
         </>
     )
