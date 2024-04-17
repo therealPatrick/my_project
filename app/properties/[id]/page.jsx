cb0909a7d465c8c97abbaba0ed1a6c98bf21441a
therealPatrick <malamashangala13@gmail.com>
2024-04-17 16:50:50 +0200

    single fetch property

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,8 +1,30 @@
 "use client";
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'next/navigation';
+import { fetchProperty } from '@/utils/requests';
 
+const PropertyPage = () => {
+    const { id } = useParams();
+    const [property, setProperty] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-const ProprtyPage = () => {
+    useEffect(() => {
+        const fetchPropertyData = async () => {
+            if (!id) return;
+            try {
+                const property = await fetchProperty(id);
+                setProperty(property);
+            } catch (error) {
+                console.error('Error fetching property:', error)
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        if (property === null) {
+            fetchPropertyData();
+        }
+    }, [id, property])
 
     return (
         <div>
@@ -11,4 +33,4 @@ const ProprtyPage = () => {
     )
 }
 
-export default ProprtyPage
+export default PropertyPage
--- utils/requests.js
+++ utils/requests.js
@@ -1,12 +1,12 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// fetch all properties
 async function fetchProperties() {
     try {
         // handle the case where the domain is not available yet
         if (!apiDomain) {
             return [];
         }
-
 
         const res = await fetch(`${apiDomain}/properties`);
         if (!res.ok) {
@@ -15,8 +15,26 @@ async function fetchProperties() {
         return res.json()
     } catch (error) {
         console.log(error)
-        return []
+        return [];
+    }
+}
+// fetch single property
+async function fetchProperty(id) {
+    try {
+        // handle the case where the domain is not available yet
+        if (!apiDomain) {
+            return null;
+        }
+
+        const res = await fetch(`${apiDomain}/properties/${id}`);
+        if (!res.ok) {
+            throw new Error('Failed to fetch data')
+        }
+        return res.json()
+    } catch (error) {
+        console.log(error)
+        return null;
     }
 }
 
-export { fetchProperties };
+export { fetchProperties, fetchProperty };
