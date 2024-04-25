bc1348b24b7fe6e492b5b63ba89e3016c851900a
therealPatrick <malamashangala13@gmail.com>
2024-04-25 16:50:33 +0200

    sign out and protect routes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -185,17 +185,26 @@ const Navbar = () => {
                                             role="menuitem"
                                             tabIndex="-1"
                                             id="user-menu-item-0"
-                                        >Your Profile</Link
-                                        >
+                                            onClick={() => {
+                                                setIsProfileMenuOpen(false)
+                                            }}
+                                        >Your Profile
+                                        </Link>
                                         <Link
                                             href="/properties/save"
                                             className="block px-4 py-2 text-sm text-gray-700"
                                             role="menuitem"
                                             tabIndex="-1"
                                             id="user-menu-item-2"
-                                        >Saved Properties</Link
-                                        >
+                                            onClick={() => {
+                                                setIsProfileMenuOpen(false)
+                                            }}
+                                        >Saved Properties</Link>
                                         <button
+                                            onClick={() => {
+                                                setIsProfileMenuOpen(false);
+                                                signOut()
+                                            }}
                                             className="block px-4 py-2 text-sm text-gray-700"
                                             role="menuitem"
                                             tabIndex="-1"
