500b6b0bbbdead932d5fd02c39604df52a8a85f7
therealPatrick <malamashangala13@gmail.com>
2024-04-25 16:34:20 +0200

    profile image showing after sign in

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,6 +10,7 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Navbar = () => {
     const { data: session } = useSession();
+    const profileImage = session?.user?.image;
 
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
@@ -23,6 +24,8 @@ const Navbar = () => {
         }
         setAuthProviders();
     }, []);
+
+
     return (
         <nav className="bg-blue-700 border-b border-blue-500">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -62,7 +65,7 @@ const Navbar = () => {
                             <Image
                                 className="h-10 w-auto"
                                 src={logo}
-                                alt="PropertyPulse"
+                                alt="realestate app"
                             />
 
                             <span className="hidden md:block text-white text-2xl font-bold ml-2">TEST</span >
@@ -159,8 +162,10 @@ const Navbar = () => {
                                         <span className="sr-only">Open user menu</span>
                                         <Image
                                             className="h-8 w-8 rounded-full"
-                                            src={profileDefault}
-                                            alt=""
+                                            src={profileImage || profileDefault}
+                                            alt="realestate app"
+                                            width={40}
+                                            height={40}
                                         />
                                     </button>
                                 </div>
